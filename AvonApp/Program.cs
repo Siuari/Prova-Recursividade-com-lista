9ed34400b9356848216b70100b323408a9ea9381
Siuari <siuaridamaceno@gmail.com>
2019-05-15 03:06:40 -0300

    ajustado método ImprimirMedia que estava iprimindo resultado inteiro

--- AvonApp/Program.cs
+++ AvonApp/Program.cs
@@ -9,7 +9,7 @@ namespace AvonApp
         public static void ImprimirMedia(int inicio, int somaParcial, int[] lista)
         {
             int somatoriaDeValoresDaLista = somaParcial;
-            double media = 0.0;
+
             //int maiorValor = 0;
             if (inicio < lista.Length)
             {
@@ -17,13 +17,16 @@ namespace AvonApp
                 ImprimirMedia(inicio + 1, somatoriaDeValoresDaLista, lista);
 
             }
-            media = somatoriaDeValoresDaLista / (double)lista.Length;
-            Console.WriteLine("A média calculada é: " + media);
-            Console.ReadLine();
+            else
+            {
+                var media = somatoriaDeValoresDaLista / (double)lista.Length;
+                Console.WriteLine("A média calculada é: " + media);
+                Console.ReadLine();
+            }
         }
         public static void Main(string[] args)
         {
-            int[] listaNumeros = { 5, 8, 2, 4, 15 };
+            int[] listaNumeros = { 5, 1, 2, 50, 15 };
             ImprimirMedia(0, 0, listaNumeros);
         }
     }
