cc7f82f255fe0829cc0b7c34607ebb5d7c8a1bfb
Siuari <siuaridamaceno@gmail.com>
2019-05-15 02:50:21 -0300

    criado metodo recursivo de imprimir media

--- AvonApp/Program.cs
+++ AvonApp/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+
 namespace AvonApp
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void ImprimirMedia(int inicio, int somaParcial, int[] lista)
         {
-            Console.WriteLine("Hello World!");
+            int somatoriaDeValoresDaLista = somaParcial;
+            double media = 0.0;
+            //int maiorValor = 0;
+            if (inicio < lista.Length)
+            {
+                somatoriaDeValoresDaLista += lista[inicio];
+                ImprimirMedia(inicio + 1, somatoriaDeValoresDaLista, lista);
+
+            }
+            media = somatoriaDeValoresDaLista / (double)lista.Length;
+            Console.WriteLine("A média calculada é: " + media);
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            int[] listaNumeros = { 5, 8, 2, 4, 15 };
+            ImprimirMedia(0, 0, listaNumeros);
         }
     }
 }
