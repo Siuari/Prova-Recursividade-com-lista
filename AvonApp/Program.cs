792a0fc41279a9700ef085ad7a79ead07cf24023
Siuari <siuaridamaceno@gmail.com>
2019-05-15 14:25:09 -0300

    criado metodo2 mais compacto para calculo recursivo da media de elementos de uma lista

--- AvonApp/Program.cs
+++ AvonApp/Program.cs
@@ -6,28 +6,49 @@ namespace AvonApp
     class Program
     {
 
-        public static void ImprimirMedia(int inicio, int somaParcial, int[] lista)
+        public static void ImprimirMedia(int inicio, int qtdInicial, int somaParcial, int[] lista)
         {
             int somatoriaDeValoresDaLista = somaParcial;
 
-            //int maiorValor = 0;
+            int qtdValoresMaiorQueMedia = qtdInicial;
+            var media = 0.0;
+
             if (inicio < lista.Length)
             {
                 somatoriaDeValoresDaLista += lista[inicio];
-                ImprimirMedia(inicio + 1, somatoriaDeValoresDaLista, lista);
-
+                ImprimirMedia(inicio + 1, qtdValoresMaiorQueMedia, somatoriaDeValoresDaLista, lista);
+                if (lista[inicio] > media)
+                    qtdValoresMaiorQueMedia++;
             }
             else
             {
-                var media = somatoriaDeValoresDaLista / (double)lista.Length;
+                media = somatoriaDeValoresDaLista / (double)lista.Length;
+
                 Console.WriteLine("A média calculada é: " + media);
+                Console.WriteLine("A quantidade de numeros maior que a média é: " + qtdValoresMaiorQueMedia);
+
                 Console.ReadLine();
             }
         }
+
+        public static void MetodoRecursivo2(int pos, int somaParcial, int[] vetor)
+        {
+            if(pos == vetor.Length - 1)
+            {
+                somaParcial += vetor[pos];
+                Console.WriteLine("Metodo recursivo 2: " + somaParcial / (double)vetor.Length);
+            } else
+            {
+                MetodoRecursivo2(pos + 1, somaParcial + vetor[pos], vetor);
+            }
+        }
+
+
         public static void Main(string[] args)
         {
             int[] listaNumeros = { 5, 1, 2, 50, 15 };
-            ImprimirMedia(0, 0, listaNumeros);
+            ImprimirMedia(0, 0, 0, listaNumeros);
+            MetodoRecursivo2(0, 0, listaNumeros);
         }
     }
 }
